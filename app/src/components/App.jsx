8001294c5a74a3969da95370e6c8bc2d74bbfdd4
Graham Hargrove <ghargrove@orisintel.com>
2018-11-03 12:36:25 -0400

    Added links

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -4,12 +4,25 @@
 
 import React from 'react';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import teal from '@material-ui/core/colors/teal'
+
 import Layout from './Layout';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[900],
+    },
+  },
+});
+
 const App = () => (
-  <Layout>
-    <div>Lets do some graphql stuff</div>
-  </Layout>
+  <MuiThemeProvider theme={theme}>
+    <Layout>
+      <div>Lets do some graphql stuff</div>
+    </Layout>
+  </MuiThemeProvider>
 );
 
 export default App;
--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -2,6 +2,10 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
@@ -9,7 +13,28 @@ import Header from './Header';
 const Layout = ({ children }) => (
   <div>
     <Header />
-    {children}
+    <Grid container justify="center" style={{ marginTop: '24px' }}>
+      <Grid item xs={9}>
+        {children}
+      </Grid>
+      <Grid item xs={3}>
+        <Typography variant="h6">
+          Links
+          <List>
+            {
+              [
+                ['React', 'https://reactjs.org/docs/hello-world.html'],
+                ['Apollo', 'https://www.apollographql.com/docs/react/essentials/get-started.html'],
+                ['GraphQL', 'https://graphql.org/learn/'],
+              ].map(([text, href], k) => (
+                <ListItem button component="a" href={href} key={k}><ListItemText primary={text} /></ListItem>
+              ))
+            }
+          </List>
+        </Typography>
+        
+      </Grid>
+    </Grid>
   </div>
 );
 
