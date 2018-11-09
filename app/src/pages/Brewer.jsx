273665831dbd705283837512f23ff672b2a2354f
Graham Hargrove <ghargrove@orisintel.com>
2018-11-09 09:10:48 -0500

    Added beer card

--- app/src/pages/Brewer.jsx
+++ app/src/pages/Brewer.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import DeleteIcon from '@material-ui/icons/Delete';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 
 import { getBrewerQuery } from '../queries';
+
+const BeerCard = ({ name, description }) => (
+  <Card style={{ marginBottom: '1em' }}>
+    <CardContent>
+      <IconButton style={{ float: 'right' }} variant="fab"><DeleteIcon /></IconButton>
+      <Typography variant="h6">{name}</Typography>
+      <Typography style={{ color: 'rgba(0, 0, 0, 0.54)' }} variant="caption">
+        {description}
+      </Typography>
+    </CardContent>
+  </Card>
+);
+
+BeerCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
+
+BeerCard.defaultProps = {
+  description: 'No description available',
+};
 
 const Brewer = ({ match: { params: { brewer: id } } }) => (
   <Grid container spacing={16}>
@@ -31,15 +56,13 @@ const Brewer = ({ match: { params: { brewer: id } } }) => (
             </Typography>
             <Grid container spacing={16} style={{ marginTop: '3em' }}>
               <Grid item xs={12}>
-                <Typography variant="display1">
+                <Typography variant="display1" style={{ marginBottom: '1em' }}>
                   Beers on Tap
                 </Typography>
-                <ul>
-                  {
-                    beers.map(beer => <li key={beer.id}>{beer.name}</li>)
-                  }
-                </ul>
               </Grid>
+              {
+                beers.map(beer => <Grid item key={beer.id} xs={6} md={4}><BeerCard {...beer}  /></Grid>)
+              }
             </Grid>
           </Grid>
         );
